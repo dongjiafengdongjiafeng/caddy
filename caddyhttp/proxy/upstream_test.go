d6745811b464816a208569c6465985dfab0d3c9a
dongjiafengdongjiafeng <48500300+dongjiafengdongjiafeng@users.noreply.github.com>
2019-04-23 16:14:16 +0800

    Update upstream_test.go

--- caddyhttp/proxy/upstream_test.go
+++ caddyhttp/proxy/upstream_test.go
@@ -30,7 +30,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/lucas-clemente/quic-go/tree/gquic/h2quic"
+	"github.com/dongjiafengdongjiafeng/quic-go/h2quic"
 	"github.com/mholt/caddy/caddyfile"
 )
 
